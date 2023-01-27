559fde886f2d7afdb63cd77e5ca3b4307040e6a2
mercedes-arnwine <80288739+mercedes-arnwine@users.noreply.github.com>
2023-01-26 23:38:55 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var shoutIt = function (name) {
+  var shout = `Hello ${name}, nice to meet you!`.toUpperCase();
+  return shout;
+};
+
+console.log(shoutIt("Doelei"));
+console.log(shoutIt("Reieko"));
+console.log(shoutIt("Ieso"));
